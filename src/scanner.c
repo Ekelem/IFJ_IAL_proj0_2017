2fa0c31e4d6a83178959cee096c80c3c4e331f2b
hersik <xsober00@stud.fit.vutbr.cz>
2017-09-25 21:12:09 +0200

    Fix in escapes sequences

--- src/scanner.c
+++ src/scanner.c
@@ -280,8 +280,8 @@ int get_token(FILE *f, token *t)
 					append_char_to_str(&ascii_seq, c);
 					state = UNUSUAL_CHAR_2;
 				}
-				else if (str_len(&ascii_seq) > 3 || c == ' ') {
-					if (c == ' ')
+				else if (str_len(&ascii_seq) == 3 || c == ' ' || c == '"' || c == '\\') {
+                    if (c == ' ' || c == '"' || c == '\\')
 						ungetc(c, f);
 
 					append_char_to_str(&s, atoi(ascii_seq.str));
