d5f3cc4913d968159052d9392dc69aa2021a0867
Erik Kelemen <xkelem01@stud.fit.vutbr.cz>
2017-10-27 18:42:43 +0200

    update main for calling parser

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@
 #include "string.h"
 #include "error.h"
 #include "symtable.h"
+#include "stack.h"
+#include "garbage_collector.h"
+#include "tokens.h"
+#include "common.h"
+#include "parser.h"
+
+struct dynamic_stack * GARBAGE_COLLECTOR = NULL;
 
 
 // AFTER TESTING LEXICAL_ANALYSIS THIS CAN BE REMOVED
@@ -16,38 +23,33 @@ char *key_words2[] = { "as", "asc", "declare", "dim", "do", "double", "else", "e
 					  "if", "input", "integer", "length", "loop", "print", "return", "scope", "string",
 					  "substr", "then", "while"};
 
+char * tok_names [] = {"LEXICAL_ERROR", "IDENTIFIER" , "AS", "ASC", "DECLARE", "DIM", "DO", "DOUBLE", "ELSE", "END", "CHR", "FUNCTION", "IF", "INPUT",
+		"INTEGER", "LENGTH", "LOOP", "PRINT", "RETURN", "SCOPE", "STRING", "SUBSTRING", "THEN", "WHILE", "AND", "BOOLEAN",
+		"CONTINUE", "ELSEIF", "EXIT", "FALSE", "FOR", "NEXT", "NOT", "OR", "SHARED", "STATIC", "TRUE", "SEMICOLON", "ADD", "SUB", "MUL", "DIV", "DIV2",
+		"LESS_THAN", "GREATER_THAN", "LESS_OR_EQUALS", "GREATER_OR_EQUALS", "EQUALS, NOT_EQUALS", "LEFT_PARANTHESIS", "RIGHT_PARANTHESIS",
+		"DOUBLEE", "INT_WITH_EXP", "DOUBLE_WITH_EXP", "INT_VALUE", "STRING_VALUE", "COMA", "KEY_WORD", "INT_2", "INT_8", "INT_16", "NEW_LINE"};
+
 /* For testing purposes */
 void print_token(int tk, token *t);
 
 int main(int argc, char const *argv[])
 {
-
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		fprintf(stderr, "Wrong arguments\n");
-		return 100;
+		return ERR_CODE_LEXICAL;
 	}
 
-	FILE *f;
-	if ((f = fopen(argv[1], "r")) == NULL) {
-		fprintf(stderr, "We could not open the file: %s\n", argv[1]);
-		return 100;
-	}
-	htab_t * symtable=htab_init(1024);
+	const char * file_path = argv[1];
+	token_buffer * token_buff = init_token_buffer();
+	htab_t * symtable = initialization(file_path, token_buff);
+	String primal_code;
 
-	int state = 0;
-	while ( state != EOF) {
-		token t;
-		t.attr.string_value = NULL;
-		state = get_token(f, &t);
-		print_token(state, &t);
-		if (t.type == IDENTIFIER || t.type == STRING_VALUE)
-			free(t.attr.string_value);
+	translate(token_buff, symtable, &primal_code);
 
-	}
-
-	printf("\n");
+	free_tokens(token_buff);
 	htab_free(symtable);
-	fclose(f);
+	garbage_collect();
 
 	return 0;
 }
